15ff352a5963fd876285cad13625a9fe9b9291c8
Jakir Hossain <jakirseu@gmail.com>
2019-11-30 12:19:11 +0400

    update project

--- app/src/main/java/me/jakir/listinintent/ArraylistActivity.java
+++ app/src/main/java/me/jakir/listinintent/ArraylistActivity.java
@@ -1,8 +1,9 @@
 package me.jakir.listinintent;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
--- app/src/main/java/me/jakir/listinintent/MainActivity.java
+++ app/src/main/java/me/jakir/listinintent/MainActivity.java
@@ -2,8 +2,9 @@ package me.jakir.listinintent;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.Serializable;
 import java.util.ArrayList;
